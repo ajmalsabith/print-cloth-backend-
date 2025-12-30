329aa36678e7fd2a2fdd96e6c4627ee908768cde
Anees <Aneesdropz@gmail.com>
2025-12-30 17:44:44 +0530

    Added user route for design fetch

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const productCtrl = require('../controllers/productController')
+const designCtrl = require('../controllers/designController')
 
 const router = express.Router();
 
+//PRODUCT
 router.get('/product', productCtrl.getAllProducts);
 router.post('/product/:id', productCtrl.getProductById);
+
+
+//DESIGN
+router.get("/design", designCtrl);
 
 module.exports = router;
