b68c77f6aa2f2681bb30e101ca9fffcf0399850c
Anees <Aneesdropz@gmail.com>
2026-02-08 23:27:13 +0530

    GuestCart implimented

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,18 +3,26 @@ const Cart = require("../models/Cart");
 const Product = require("../models/product");
 const User = require("../models/User");
 const { NotFoundError, ValidationError } = require("../utils/errors");
+const logger = require("../utils/logger");
+const { sendError } = require("../utils/response");
 const { sendSuccess } = require("./BaseController");
 
 //FETCH CART ITEMS
   async function fetchCart (req, res) {
+    let cart
     try {
 
-      const userId = req.user._id.toString();
-console.log('in fetch cart server');
+      const userId = req?.user?._id.toString();
+      const guestId = req.query?.guestId
 
-      const cart = await Cart.findOne({ user: userId }).populate(
+      if (userId) {
+        cart = await Cart.findOne({ user: userId }).populate(
         "items.product")
-console.log('fetcged', cart);
+      }
+      else if (guestId) {
+        cart = await Cart.findOne({ guestId: req.query.guestId }).populate(
+        "items.product");
+      }
 
       sendSuccess(res, 'Cart fetch successful', {cart}, 200)
     } catch (error) {
@@ -78,9 +86,10 @@ console.log('fetcged', cart);
   const addToCart = async(req, res) => {
 
     const { productId, quantity=1, color, size } = req.body
-    console.log('product details:', req.body)
     
-    const userId = req.user._id
+    const userId = req.user?._id
+    const guestId = req.query?.guestId
+    let cart
     const warnings = []
     try {
       const product = await Product.findById(productId);
@@ -93,14 +102,17 @@ console.log('fetcged', cart);
       const itemTotal = finalUnitPrice * quantity;
 
       //find correct user cart
-      let cart = await Cart.findOne({ user: userId })
-console.log('cart', cart);
-
+      if (userId) {
+        cart = await Cart.findOne({ user: userId })
+      }
+      else {
+        cart = await Cart.findOne({ guestId })
+      }
 
       //if no cart -> create one
       if (!cart) {
         cart = new Cart({
-          user: userId,
+          ...(userId ? {user: userId} : {guestId}),
           items: [
             {
               product: productId,
@@ -121,8 +133,6 @@ console.log('cart', cart);
         );
         const matchingColor = cart.items[itemIndex]?.color === color
         const matchingSize = cart.items[itemIndex]?.size === size
-console.log('matching color:', cart.items[itemIndex]?.color, color);
-console.log('matching color:', cart.items[itemIndex]?.size, size);
 
         //product exist in cart-> update quantity + itemTotal
         if (itemIndex > -1 && matchingColor && matchingSize) {
@@ -167,17 +177,29 @@ console.log('matching color:', cart.items[itemIndex]?.size, size);
   const removeFromCart = async(req, res) => {
     try {
         const warnings = []
-        const userId = req.user._id
+        const userId = req.user?._id
+        const guestId = req.query?.guestId
         const { _id, color, size } = req.body
-        console.log();
+        let cart
         
-      const cart = await Cart.findOneAndUpdate(
+        if (userId) {
+          cart = await Cart.findOneAndUpdate(
         { user: userId },
         { $pull: { items: { _id, color, size } } },
         {
           new: true,
         }
       ).populate("items.product")
+        }
+        else {
+         cart = await Cart.findOneAndUpdate(
+        { guestId },
+        { $pull: { items: { _id, color, size } } },
+        {
+          new: true,
+        }
+      ).populate("items.product")
+        }
 
       if (!cart) {
         throw new NotFoundError("Product not found", 404);
@@ -197,8 +219,10 @@ console.log('matching color:', cart.items[itemIndex]?.size, size);
 
   //SYNC CART -> ADD/DECREASE QUANTITY
   const syncCart = async(req, res) => {
-    const userId = req.user._id
+    const userId = req.user?._id
+    const guestId = req.query?.guestId
     const items = req.body
+    let cart
     
     try {
       if (!Array.isArray(items)) {
@@ -262,8 +286,12 @@ console.log('reqstd', requestedQuantity);
         })
         .filter(Boolean)
 
-      const cart = await Cart.findOne({ user: userId })
-
+      if (userId) {
+        cart = await Cart.findOne({ user: userId })
+      }
+      else {
+        cart = await Cart.findOne({ guestId })
+      }
       if (!cart) throw new NotFoundError("Cart not found");
 
       cart.items = cartItems;
@@ -288,14 +316,19 @@ console.log('reqstd', requestedQuantity);
   const updateSize = async (req, res) => {
   try {
     const { itemId, productId, size, color } = req.body;
-    const userId = req.user._id;
+    const userId = req.user?._id;
+    const guestId = req.query?.guestId;
+    let cart
 
-    const cart = await Cart.findOne({ user: userId });
+    if (userId) {
+       cart = await Cart.findOne({ user: userId });
+    }
+    else {
+       cart = await Cart.findOne({ guestId });
+    }
     if (!cart) return res.status(404).json({ message: "Cart not found" });
 
     // Find item being updated
-    console.log('car items::', cart.items);
-    
     const itemIndex = cart.items.findIndex(
       (i) => i._id.toString() === itemId
     );
@@ -323,7 +356,7 @@ console.log('reqstd', requestedQuantity);
 
       //if product variant doesnt exist
     } else {
-      // ✏️ Just update size
+      // Just update size
       cart.items[itemIndex].size = size;
     }
 
@@ -377,10 +410,75 @@ console.log('reqstd', requestedQuantity);
 //     return {cart}
 //   }
 
+//MERGE CART
+const mergeCart = async(req, res) => {
+  
+  try {
+    const userId = req.user?._id
+    const guestId = req.query?.guestId
+    console.log('in merge cart', userId, guestId);
+  let cart
+
+  if (!userId || !guestId) {
+      return res.status(400).json({ message: "Invalid merge request" })
+    }
+
+  
+    const userCart = await Cart.findOne({ user: userId })
+    const guestCart = await Cart.findOne({ guestId })
+
+     //No guestCart = Nothing to merge
+    if (!guestCart || guestCart.items.length === 0) {
+      return res.status(200).json({ cart: userCart })
+    }
+
+    // If user has no cart -> convert guest cart
+    if (!userCart) {
+      guestCart.user = userId
+      guestCart.guestId = undefined
+      await guestCart.save()
+
+      return res.status(200).json({ cart: guestCart })
+    }
+
+    // Merge items
+    guestCart.items.forEach(guestItem => {
+      const existingItem = userCart.items.find(userItem =>
+        userItem.product.toString() === guestItem.product.toString() &&
+        userItem.size === guestItem.size &&
+        userItem.color === guestItem.color
+      )
+
+      if (existingItem) {
+        existingItem.quantity += guestItem.quantity
+        existingItem.itemTotal =
+          existingItem.quantity * existingItem.finalUnitPrice
+      } else {
+        userCart.items.push(guestItem)
+      }
+    })
+
+    
+    await userCart.save()
+    
+    //Remove guestCart
+    await Cart.deleteOne({ guestId })
+    
+    await userCart.populate('items.product')
+
+    sendSuccess(res, 'Cart merged successfully', {cart: userCart}, 200)
+
+  } catch (error) {
+    console.error('MergeCart error:', error)
+    throw error
+  }
+  }
+
 module.exports = {
     fetchCart,
     addToCart,
     syncCart,
     removeFromCart,
-    updateSize
+    updateSize,
+    mergeCart
 };
--- routes/cart.js
+++ routes/cart.js
@@ -1,16 +1,15 @@
 const express = require('express')
-const { authenticateUser } = require('../middlewares/auth')
+const { authenticateUser, authenticateCart } = require('../middlewares/auth')
 const CartController = require('../controllers/cartController')
 
 const router = express.Router()
 
 //cart api handlers
-router.get('/', authenticateUser, CartController.fetchCart)
-router.post('/', authenticateUser, CartController.addToCart)
-router.put('/', authenticateUser, CartController.syncCart)
-router.put('/size', authenticateUser, CartController.updateSize)
-router.delete('/', authenticateUser, CartController.removeFromCart)
-
-
+router.get('/', authenticateCart, CartController.fetchCart)
+router.post('/', authenticateCart, CartController.addToCart)
+router.put('/', authenticateCart, CartController.syncCart)
+router.put('/size', authenticateCart, CartController.updateSize)
+router.delete('/', authenticateCart, CartController.removeFromCart)
+router.post('/merge', authenticateCart, CartController.mergeCart)
 
 module.exports = router
