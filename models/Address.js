bd45ca84c19605b48ee266c52732c368edf0e82a
sanojkrishnan <sanojkrishnan12@gmail.com>
2026-03-12 23:22:17 +0530

    date 12/3/2026

---
+++ models/Address.js
@@ -0,0 +1,56 @@
+const { ObjectId } = require('mongodb')
+const mongoose = require('mongoose')
+
+const CartSchema = mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    label: {
+        type: String
+    },
+    fullName: {
+        type: String
+    },
+    phoneNumber: {
+        type: Number
+    },
+     addressLine1: {
+        type: String
+    },
+     addressLine2: {
+        type: String
+    },
+     city: {
+        type: String
+    },
+     state: {
+        type: String
+    },
+     postalCode: {
+        type: String
+    },
+     country: {
+        type: String
+    },
+     isDefaultShipping: {
+        type: Boolean
+    },
+     isDefaultBilling: {
+        type: Boolean
+    },
+     createdAt: {
+        type: Date
+    },
+     updatedAt: {
+        type: Date
+    },
+   
+},{timestamps: true})
+
+
+module.exports = mongoose.model('Cart', CartSchema)
+
+
+
